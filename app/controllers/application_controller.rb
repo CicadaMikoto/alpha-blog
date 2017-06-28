bc9b37dfc0c493099caac052265185dbfd830c72
CicadaMikoto <geraldmroybal@gmail.com>
2017-06-27 23:11:57 -0400

    Login/Logout Implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in? # available to views
+
+  def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id] # if exists or not
+  end
+
+  def logged_in?
+      !!current_user # boolean conversion
+  end
+
+  def require_user
+      if !logged_in?
+          flash[:danger] = "You must not sneak"
+          redirect_to root_path
+      end
+  end
 end
